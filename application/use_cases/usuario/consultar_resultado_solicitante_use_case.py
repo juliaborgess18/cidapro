fda7144c08097e8b9487004989af0b690d925359
Igor <Igor.almeidadasilva13@gmail.com>
2024-06-30 20:37:52 -0300

    hotfix: consultar resultado

--- application/use_cases/usuario/consultar_resultado_solicitante_use_case.py
+++ application/use_cases/usuario/consultar_resultado_solicitante_use_case.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from domain.entities.solicitacao import Solicitacao
+from domain.errors.NotFoundException import NotFoundException
 from infrastructure.repositories.solicitacao_repo import SolicitacaoRepo
 
 
@@ -8,5 +9,12 @@ class ConsultarResultadoSolicitanteUseCase:
     
     @classmethod
     async def execute(cls, id_solicitacao:str, id_usuario: str) -> Optional[Solicitacao]:   
-        solicitacao = SolicitacaoRepo.selecionar_por_id_e_usuario_logado(id_solicitacao, id_usuario)
+        solicitacao = None
+
+        if id_solicitacao != "":
+            solicitacao = SolicitacaoRepo.selecionar_por_id_e_usuario_logado(id_solicitacao, id_usuario)
+
+            if solicitacao == None:
+                raise NotFoundException("Solicitação não encontrada")
+
         return solicitacao 
--- presentation/routes/usuario/usuario_html_routes.py
+++ presentation/routes/usuario/usuario_html_routes.py
@@ -1,6 +1,6 @@
 import datetime
 from typing import List
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response, status
 from fastapi.responses import HTMLResponse
 from application.mapper.solicitacao_mapper import SolicitacaoMapper
 from application.mapper.usuario_mapper import UsuarioMapper
@@ -8,6 +8,7 @@ from application.use_cases.usuario.analisar_solicitacao_use_case import Analisar
 from application.use_cases.usuario.consultar_resultado_solicitante_use_case import ConsultarResultadoSolicitanteUseCase
 from application.use_cases.usuario.visualizar_historico_solicitante import VisualizarHistoricoSolicitacoesUseCase
 from application.use_cases.usuario.visualizar_solicitante_use_case import VisualizarSolicitanteUseCase
+from application.utils.cookies import adicionar_mensagem_sucesso
 from infrastructure.repositories.motivo_repo import MotivoRepo
 from domain.errors.NotFoundException import NotFoundException
 from infrastructure.repositories.pais_repo import PaisRepo
@@ -30,9 +31,15 @@ async def get_solicitante_nova_solicitacao(request: Request):
     return templates.TemplateResponse("solicitante/nova_solicitacao.html", {"request": request, "paises":paises, "motivos":motivos})
 
 @router.get("/usuario/solicitante/consultar_solicitacao", response_class=HTMLResponse)
-async def get_solicitante_consultar_solicitacao(request: Request, id_solicitacao: str="", id_usuario: str = ""):
+async def get_solicitante_consultar_solicitacao(request: Request, id_solicitacao: str = "", id_usuario: str = ""):
+
     solicitacao = await ConsultarResultadoSolicitanteUseCase.execute(id_solicitacao, id_usuario)
-    return templates.TemplateResponse("solicitante/consultar_resultado.html", {"request": request, "solicitacao": solicitacao})
+
+    return templates.TemplateResponse("solicitante/consultar_resultado.html", 
+    {
+        "request": request, 
+        "solicitacao": solicitacao
+    })
 
 @router.get("/usuario/solicitante/historico_solicitacao/{id_usuario}", response_class=HTMLResponse)
 async def get_pagina_inicial_solicitante(request: Request, id_usuario: str):
